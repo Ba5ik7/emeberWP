ae2b49010707994bf8b0eb687ec2e488d4c28170
Wesley Balls <dusellfam@gmail.com>
2014-12-23 17:13:02 -0500

    Added a very simple error handling action on the appication route

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -1,5 +1,17 @@
 Emberwp.ApplicationRoute = Ember.Route.extend({
 
+    actions: {
+
+        error: function(error, transition) {
+
+            // Manage your errors
+            Ember.onerror(error);
+
+            // substate implementation when returning `true`
+            return true;
+        }
+    },
+
     model: function(model) {
 
         var self = this;
