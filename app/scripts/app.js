b10a042d02cecdcbe02cdf0975141ddb512391e1
Wesley Balls <dusellfam@gmail.com>
2014-12-25 20:25:13 -0500

    Added the check box side nav hack

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,8 @@
-toastr.options = {
-  "closeButton": false,
-  "debug": false,
-  "progressBar": true,
-  "positionClass": "toast-bottom-center",
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
+toastr.options = {   "closeButton": false,   "debug": false,   "progressBar":
+true,   "positionClass": "toast-bottom-center",   "onclick": null,
+"showDuration": "300",   "hideDuration": "1000",   "timeOut": "5000",
+"extendedTimeOut": "1000",   "showEasing": "swing",   "hideEasing": "linear",
+"showMethod": "fadeIn",   "hideMethod": "fadeOut" }
 
 var Emberwp = window.Emberwp = Ember.Application.create();
 
--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -2,11 +2,11 @@ Emberwp.ApplicationView = Ember.View.extend({
 
     didInsertElement: function() {
         
-            $('nav ul li').css('margin-top', '-50px');
+            $('nav ul li').css({'margin-top': '-50px', 'opacity': '0'});
     },
 
     animateIn : function (done) {
-            TweenMax.staggerTo("nav ul li", .5, {force3D:true,  marginTop:'0', ease:Back.easeOut }, .1, done);
+            TweenMax.staggerTo("nav ul li", .5, {force3D:true,  marginTop:'0', ease:Back.easeOut, opacity:1 }, .1, done);
     },
 
     animateOut : function (done) {
